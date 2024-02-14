e571bd892ade89ebfb168ea95428c2098ce71778
Outurnate <joseph@outurnate.com>
2024-02-13 20:02:37 -0500

    archiving for windows and linux, MOAR TOOLS

--- build.sh
+++ build.sh
@@ -1,32 +1,94 @@
 #!/bin/bash
-mkdir -p ~/payloads/{lin,win}/{32,64,shell} ~/payloads/win/msil/{2,3,4}
+OUTPUT=$HOME/paylods
+
+rm $OUTPUT -rf
+
+mkdir -p $OUTPUT/{lin,win}/{32,64,shell} $OUTPUT/win/msil/{2,3,4}
 pushd src
-DOCKER_BUILDKIT=1 docker build --build-arg docker_image=i386/alpine:latest --output type=local,dest=$HOME/payloads/lin/32 .
-DOCKER_BUILDKIT=1 docker build --build-arg docker_image=alpine:latest      --output type=local,dest=$HOME/payloads/lin/64 .
+DOCKER_BUILDKIT=1 docker build --build-arg docker_image=i386/alpine:latest --output type=local,dest=$OUTPUT/lin/32 .
+DOCKER_BUILDKIT=1 docker build --build-arg docker_image=alpine:latest      --output type=local,dest=$OUTPUT/lin/64 .
 popd
 
-curl -L https://github.com/ly4k/PwnKit/raw/main/PwnKit > ~/payloads/lin/64/pwnkit
-curl -L https://github.com/ly4k/PwnKit/raw/main/PwnKit32 > ~/payloads/lin/32/pwnkit
-curl -L https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas_linux_amd64 > ~/payloads/lin/64/linpeas
-curl -L https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas_linux_386 > ~/payloads/lin/32/linpeas
-curl -L https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas.sh > ~/payloads/lin/shell/linpeas.sh
-curl -L https://github.com/carlospolop/PEASS-ng/releases/latest/download/winPEASx64.exe > ~/payloads/win/64/winpeas.exe
-curl -L https://github.com/carlospolop/PEASS-ng/releases/latest/download/winPEASx86.exe > ~/payloads/win/32/winpeas.exe
-curl -L https://github.com/carlospolop/PEASS-ng/releases/latest/download/winPEAS.bat > ~/payloads/win/shell/winpeas.bat
-curl -L https://github.com/BeichenDream/GodPotato/releases/download/V1.20/GodPotato-NET2.exe > ~/payloads/win/msil/2/godpotato.exe
-curl -L https://github.com/BeichenDream/GodPotato/releases/download/V1.20/GodPotato-NET35.exe > ~/payloads/win/msil/3/godpotato.exe
-curl -L https://github.com/BeichenDream/GodPotato/releases/download/V1.20/GodPotato-NET4.exe > ~/payloads/win/msil/4/godpotato.exe
-cp src/PwnKit.sh ~/payloads/lin/shell/pwnkit.sh
-cp src/gameoverlay.sh ~/payloads/lin/shell/gameoverlay.sh
+fetch() {
+  wget -q --show-progress -O $1 $2
+}
 
-chmod -R +x ~/payloads/lin
+pushd $OUTPUT/lin/64 > /dev/null
+fetch pwnkit https://github.com/ly4k/PwnKit/raw/main/PwnKit
+fetch linpeas https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas_linux_amd64
+popd > /dev/null
 
-tar -zcvf ~/payloads/lin64.tar.gz -C ~/payloads/lin/64 $(ls ~/payloads/lin/64) -C ~/payloads/lin/shell $(ls ~/payloads/lin/shell)
-tar -zcvf ~/payloads/lin32.tar.gz -C ~/payloads/lin/32 $(ls ~/payloads/lin/32) -C ~/payloads/lin/shell $(ls ~/payloads/lin/shell)
-rm ~/payloads/win{64,32}.zip 2> /dev/null
-zip -j ~/payloads/win64.zip ~/payloads/win/64/* ~/payloads/win/shell/*
-zip -j ~/payloads/win32.zip ~/payloads/win/32/* ~/payloads/win/shell/*
-cd ~/payloads/win && zip -r ~/payloads/win64.zip msil
-cd ~/payloads/win && zip -r ~/payloads/win32.zip msil
+pushd $OUTPUT/lin/32 > /dev/null
+fetch pwnkit https://github.com/ly4k/PwnKit/raw/main/PwnKit32
+fetch linpeas https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas_linux_386
+popd > /dev/null
 
-tree ~/payloads -h
+pushd $OUTPUT/lin/shell > /dev/null
+fetch linpeas.sh https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas.sh
+popd > /dev/null
+
+pushd $OUTPUT/win/64 > /dev/null
+fetch winpeas.exe https://github.com/carlospolop/PEASS-ng/releases/latest/download/winPEASx64.exe
+fetch mimidrv.sys https://github.com/ParrotSec/mimikatz/raw/master/x64/mimidrv.sys
+fetch mimikatz.exe https://github.com/ParrotSec/mimikatz/raw/master/x64/mimikatz.exe
+fetch mimilib.dll https://github.com/ParrotSec/mimikatz/raw/master/x64/mimilib.dll
+popd > /dev/null
+
+pushd $OUTPUT/win/32 > /dev/null
+fetch winpeas.exe https://github.com/carlospolop/PEASS-ng/releases/latest/download/winPEASx86.exe
+fetch mimidrv.exe https://github.com/ParrotSec/mimikatz/raw/master/Win32/mimidrv.sys
+fetch mimikatz.exe https://github.com/ParrotSec/mimikatz/raw/master/Win32/mimikatz.exe
+fetch mimilib.dll https://github.com/ParrotSec/mimikatz/raw/master/Win32/mimilib.dll
+popd > /dev/null
+
+pushd $OUTPUT/win/shell > /dev/null
+fetch winpeas.bat https://github.com/carlospolop/PEASS-ng/releases/latest/download/winPEAS.bat
+fetch powerview.ps1 https://github.com/PowerShellMafia/PowerSploit/raw/master/Recon/PowerView.ps1
+fetch powerup.ps1 https://github.com/PowerShellMafia/PowerSploit/raw/master/Privesc/PowerUp.ps1
+popd > /dev/null
+
+pushd $OUTPUT/win/msil/2 > /dev/null
+fetch godpotato.exe https://github.com/BeichenDream/GodPotato/releases/download/V1.20/GodPotato-NET2.exe
+popd > /dev/null
+
+pushd $OUTPUT/win/msil/3 > /dev/null
+fetch godpotato.exe https://github.com/BeichenDream/GodPotato/releases/download/V1.20/GodPotato-NET35.exe
+fetch rubeus.exe https://github.com/r3motecontrol/Ghostpack-CompiledBinaries/raw/master/dotnet%20v3.5%20compiled%20binaries/Rubeus.exe
+fetch seatbelt.exe https://github.com/r3motecontrol/Ghostpack-CompiledBinaries/raw/master/dotnet%20v3.5%20compiled%20binaries/Seatbelt.exe
+popd > /dev/null
+
+pushd $OUTPUT/win/msil/4 > /dev/null
+fetch godpotato.exe https://github.com/BeichenDream/GodPotato/releases/download/V1.20/GodPotato-NET4.exe
+fetch rubeus.exe https://github.com/r3motecontrol/Ghostpack-CompiledBinaries/raw/master/dotnet%20v4.5%20compiled%20binaries/Rubeus.exe
+fetch seatbelt.exe https://github.com/r3motecontrol/Ghostpack-CompiledBinaries/raw/master/dotnet%20v4.5%20compiled%20binaries/Seatbelt.exe
+popd > /dev/null
+
+cp src/PwnKit.sh $OUTPUT/lin/shell/pwnkit.sh
+cp src/gameoverlay.sh $OUTPUT/lin/shell/gameoverlay.sh
+
+chmod -R +x $OUTPUT/lin
+
+for ARCH in 64 32
+do
+  for PLATFORM in lin win
+  do
+    for f in $(ls $OUTPUT/$PLATFORM/shell)
+    do
+      ln -T $OUTPUT/$PLATFORM/shell/$f $OUTPUT/$PLATFORM/$ARCH/$f
+    done
+  done
+
+  for VERSION in 2 3 4
+  do
+    for f in $(ls $OUTPUT/win/msil/$VERSION/*.exe)
+    do
+      ln -T $OUTPUT/win/msil/$VERSION/$(basename "$f") $OUTPUT/win/$ARCH/$(basename "$f" .exe)-msil$VERSION.exe
+    done
+  done
+
+  cd $OUTPUT/lin/$ARCH && tar -zcf $OUTPUT/lin$ARCH.tar.gz *
+  zip -j $OUTPUT/win$ARCH.zip $OUTPUT/win/$ARCH/*
+done
+
+tree $OUTPUT -h
+du -hd0 $OUTPUT
